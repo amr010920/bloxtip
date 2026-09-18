0e8979c18009a6c0f722aa3cdd236be28e86c23d
amr010920 <hassanelsharkawy855@gmail.com>
2026-09-18 11:46:14 +0300

    Make the Play button click launch the bloxtip protocol and tighten the BAT launcher behavior.

--- launch_bloxtip.bat
+++ launch_bloxtip.bat
@@ -1,11 +1,16 @@
 @echo off
 setlocal
 
-rem Run launch_bloxtip.bat "place-file.rbxl" to send a place to the installed client.
+rem Usage:
+rem   launch_bloxtip.bat "C:\path\to\place-file.rbxl"
+rem   launch_bloxtip.bat
+
 if "%~1"=="" (
   start "" "https://amr010920.github.io/bloxtip/home/"
-) else (
-  start "" "bloxtip://play?place=%~1"
+  exit /b
 )
+
+set "place=%~1"
+start "" "bloxtip://play?place=%place%"
 
 endlocal
