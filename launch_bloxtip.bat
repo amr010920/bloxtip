ad4c71a7f793d713521be88ffa8bcc17cf5ad122
amr010920 <hassanelsharkawy855@gmail.com>
2026-09-18 11:30:53 +0300

    Pass a selected place to the bloxtip client launcher

--- launch_bloxtip.bat
+++ launch_bloxtip.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal
 
-start "" "https://amr010920.github.io/bloxtip/"
+rem Run launch_bloxtip.bat "place-file.rbxl" to send a place to the installed client.
+if "%~1"=="" (
+  start "" "https://amr010920.github.io/bloxtip/home/"
+) else (
+  start "" "bloxtip://play?place=%~1"
+)
 
 endlocal
